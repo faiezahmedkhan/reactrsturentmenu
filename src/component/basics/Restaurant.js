193b553342cbdfa386c2ebcab2aa0ca3c6847391
unknown <faiez844@gmail.com>
2022-04-11 14:46:25 +0530

    initial commit

---
+++ src/component/basics/Restaurant.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import "./style.css";
+import Menu from "./menuApi.js";
+import MenuCard from "./MenuCard";
+import Navbar from "./navbar";
+
+
+const uniqueList = [
+  ...new Set(Menu.map((curElem) =>{
+ 
+   return curElem.category;
+
+})
+),
+
+ "All",
+
+];
+
+console.log(uniqueList);
+
+
+
+const Restaurant = () => {
+  
+   const [menuData, setMenudata] = useState(Menu);
+   const [menuList, setMenuList] = useState(uniqueList);
+
+   const filterItem = (category) => {
+
+   if(category === 'All'){
+     setMenudata(Menu);
+     return;
+   }
+     
+
+     const updatedList = Menu.filter((curElem)=>{
+ 
+        return curElem.category === category;
+ 
+    });
+
+    
+
+    setMenudata(updatedList);
+     
+
+   };
+
+
+  return (
+    <>
+
+
+    <Navbar filterItem={filterItem} menuList={menuList}/>
+    <MenuCard menuData={menuData}/>
+  
+  
+   </>
+  );
+};
+
+export default Restaurant;
